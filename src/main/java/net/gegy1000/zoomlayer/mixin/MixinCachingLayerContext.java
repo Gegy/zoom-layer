7f7dd39a9bdee74146c1dbbfe0b4086bac5050f9
gegy1000 <gegy1000@gmail.com>
2020-06-08 14:13:06 +0200

    attempt at thread-local biome layers

--- src/main/java/net/gegy1000/zoomlayer/mixin/MixinCachingLayerContext.java
+++ src/main/java/net/gegy1000/zoomlayer/mixin/MixinCachingLayerContext.java
@@ -1,31 +1,23 @@
 package net.gegy1000.zoomlayer.mixin;
 
-import net.gegy1000.zoomlayer.ConcurrentLayerCache;
+import net.gegy1000.zoomlayer.CachingLayerAccess;
 import net.gegy1000.zoomlayer.FastCachingLayerSampler;
 import net.minecraft.world.biome.layer.util.CachingLayerContext;
 import net.minecraft.world.biome.layer.util.CachingLayerSampler;
 import net.minecraft.world.biome.layer.util.LayerOperator;
 import net.minecraft.world.biome.source.SeedMixer;
-
-import net.gegy1000.zoomlayer.CachingLayerAccess;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CachingLayerContext.class)
 public class MixinCachingLayerContext implements CachingLayerAccess {
-    @Shadow private long localSeed;
-    @Shadow @Final private long worldSeed;
-    private ConcurrentLayerCache fastCache;
-
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void init(int capacity, long seed, long salt, CallbackInfo ci) {
-        this.fastCache = new ConcurrentLayerCache(512);
-    }
+    @Shadow
+    private long localSeed;
+    @Shadow
+    @Final
+    private long worldSeed;
 
     /**
      * Replace with optimized implementation
@@ -34,7 +26,7 @@ public class MixinCachingLayerContext implements CachingLayerAccess {
      */
     @Overwrite
     public CachingLayerSampler createSampler(LayerOperator operator) {
-        return new FastCachingLayerSampler(this.fastCache, operator);
+        return new FastCachingLayerSampler(128, operator);
     }
 
     /**
@@ -44,7 +36,7 @@ public class MixinCachingLayerContext implements CachingLayerAccess {
      */
     @Overwrite
     public CachingLayerSampler createSampler(LayerOperator operator, CachingLayerSampler sampler) {
-        return new FastCachingLayerSampler(this.fastCache, operator);
+        return new FastCachingLayerSampler(128, operator);
     }
 
     /**
@@ -54,7 +46,7 @@ public class MixinCachingLayerContext implements CachingLayerAccess {
      */
     @Overwrite
     public CachingLayerSampler createSampler(LayerOperator operator, CachingLayerSampler left, CachingLayerSampler right) {
-        return new FastCachingLayerSampler(this.fastCache, operator);
+        return new FastCachingLayerSampler(128, operator);
     }
 
     @Override
