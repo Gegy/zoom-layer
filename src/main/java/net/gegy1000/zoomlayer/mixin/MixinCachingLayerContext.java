db6258b4769f188165872d7de99b7640a9ad8bd0
gegy1000 <gegy1000@gmail.com>
2020-06-20 21:47:30 +0200

    reduced implementation

--- src/main/java/net/gegy1000/zoomlayer/mixin/MixinCachingLayerContext.java
+++ src/main/java/net/gegy1000/zoomlayer/mixin/MixinCachingLayerContext.java
@@ -1,24 +1,14 @@
 package net.gegy1000.zoomlayer.mixin;
 
-import net.gegy1000.zoomlayer.CachingLayerAccess;
 import net.gegy1000.zoomlayer.FastCachingLayerSampler;
 import net.minecraft.world.biome.layer.util.CachingLayerContext;
 import net.minecraft.world.biome.layer.util.CachingLayerSampler;
 import net.minecraft.world.biome.layer.util.LayerOperator;
-import net.minecraft.world.biome.source.SeedMixer;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(CachingLayerContext.class)
-public class MixinCachingLayerContext implements CachingLayerAccess {
-    @Shadow
-    private long localSeed;
-    @Shadow
-    @Final
-    private long worldSeed;
-
+public class MixinCachingLayerContext {
     /**
      * Replace with optimized implementation
      *
@@ -47,10 +37,5 @@ public class MixinCachingLayerContext implements CachingLayerAccess {
     @Overwrite
     public CachingLayerSampler createSampler(LayerOperator operator, CachingLayerSampler left, CachingLayerSampler right) {
         return new FastCachingLayerSampler(128, operator);
-    }
-
-    @Override
-    public void skipInt() {
-        this.localSeed = SeedMixer.mixSeed(this.localSeed, this.worldSeed);
     }
 }
